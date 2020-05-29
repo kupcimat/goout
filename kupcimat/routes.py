dd41f165f05c9ee80b03fc9d01fe037cf1ba6469
Matej Kupciha <kupciham@hotmail.com>
2020-05-29 11:07:53 +0200

    Add cloud storage methods

--- kupcimat/routes.py
+++ kupcimat/routes.py
@@ -1,9 +1,38 @@
+import uuid
+from typing import List
+
 from aiohttp import web
 
+from kupcimat import storage
 
-def create_routes():
-    return [web.get("/", hello)]
+BUCKET_NAME = "goout-test"
 
 
-async def hello(request):
-    return web.Response(text="Hello, world")
+def create_routes() -> List[web.RouteDef]:
+    return [
+        web.get("/api/files/uploadLink", get_upload_url),
+        web.get("/api/files/{file_name}/downloadLink", get_download_url)
+    ]
+
+
+async def get_upload_url(request: web.Request) -> web.Response:
+    url = storage.generate_upload_signed_url(BUCKET_NAME, uuid.uuid4().hex)
+    response = {
+        "upload": {
+            "url": url,
+            "curl": storage.generate_upload_curl(url)
+        }
+    }
+    return web.json_response(response)
+
+
+async def get_download_url(request: web.Request) -> web.Response:
+    file_name = request.match_info["file_name"]
+    url = storage.generate_download_signed_url(BUCKET_NAME, file_name)
+    response = {
+        "download": {
+            "url": url,
+            "curl": storage.generate_download_curl(url)
+        }
+    }
+    return web.json_response(response)
---
+++ kupcimat/storage.py
@@ -0,0 +1,50 @@
+import datetime
+import logging
+
+from google.cloud import storage
+
+SIGNED_URL_EXPIRATION_MINUTES = 5
+FILE_SIZE_LIMIT_BYTES = 1 * 1024 * 1024
+
+
+def generate_download_signed_url(bucket_name: str, blob_name: str) -> str:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        method="GET",
+        expiration=datetime.timedelta(minutes=SIGNED_URL_EXPIRATION_MINUTES)
+    )
+
+    logging.info("action=generate_storage_url type=download bucket=%s blob=%s", bucket_name, blob_name)
+    return url
+
+
+def generate_upload_signed_url(bucket_name: str, blob_name: str) -> str:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        method="PUT",
+        content_type="application/octet-stream",
+        headers={"x-goog-content-length-range": f"0,{FILE_SIZE_LIMIT_BYTES}"},
+        expiration=datetime.timedelta(minutes=SIGNED_URL_EXPIRATION_MINUTES)
+    )
+
+    logging.info("action=generate_storage_url type=upload bucket=%s blob=%s", bucket_name, blob_name)
+    return url
+
+
+def generate_download_curl(url: str) -> str:
+    return f"curl '{url}'"
+
+
+def generate_upload_curl(url: str) -> str:
+    return ("curl -X PUT "
+            "-H 'Content-Type: application/octet-stream' "
+            f"-H 'x-goog-content-length-range: 0,{FILE_SIZE_LIMIT_BYTES}' "
+            f"--upload-file file '{url}'")
