5da75540f761a7f4987618e95f340042822c34f0
Matej Kupciha <kupciham@hotmail.com>
2020-06-01 09:21:14 +0200

    Add simple worker api

--- kupcimat/worker/routes.py
+++ kupcimat/worker/routes.py
@@ -2,12 +2,35 @@ from typing import List
 
 from aiohttp import web
 
+from kupcimat.routes_util import created, path_variables
+
 
 def create_routes() -> List[web.RouteDef]:
     return [
-        web.get("/api", hello)
+        web.post("/api/tasks", create_task),
+        web.get("/api/tasks/{task_id}", get_task)
     ]
 
 
-async def hello(request: web.Request) -> web.Response:
-    return web.json_response({})
+async def create_task(request: web.Request) -> web.Response:
+    # TODO validate json input
+    task_json = await request.json()
+    task_id = task_json["task"]["id"]
+    response = {
+        "task": {
+            "id": task_id
+        }
+    }
+    return web.json_response(response, **created(f"/api/tasks/{task_id}"))
+
+
+async def get_task(request: web.Request) -> web.Response:
+    # TODO check task existence
+    task_id = path_variables(request, "task_id")
+    response = {
+        "task": {
+            "id": task_id,
+            "result": 42
+        }
+    }
+    return web.json_response(response)
