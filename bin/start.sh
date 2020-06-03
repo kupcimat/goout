54f4e2245faf0ad74f35f9054c00f3953eb91d8f
Matej Kupciha <kupciham@hotmail.com>
2020-06-03 08:17:26 +0200

    Remove old deployment scripts

--- bin/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-set -ex
-
-HEROKU_APP=$1
-
-# Start worker on heroku
-heroku ps:scale worker=1 --app=${HEROKU_APP}
--- bin/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-set -ex
-
-HEROKU_APP=$1
-
-# Stop worker on heroku
-heroku ps:scale worker=0 --app=${HEROKU_APP}
